c5ff7212dec49c59f82d064bd777f96394e81c67
Ericx44 <eric.deguzman0012@gmail.com>
2023-08-11 17:28:00 +0800

    second push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <script setup>
 import UserTable from '../components/HomeView/UserTable.vue';
-
-
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faRightFromBracket, faUserTag, faUserClock, faUserCheck, faUserGroup, faCamera } from '@fortawesome/free-solid-svg-icons'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+library.add(faCamera)
+library.add(faUserGroup)
+library.add(faUserCheck)
+library.add(faUserClock)
+library.add(faUserTag)
+library.add(faCircleExclamation)
 </script>
 
 <template>
@@ -14,7 +23,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">Pending Invites</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+       <font-awesome-icon  :icon="faUserClock"  class="fa-solid fa-user mt-5 h-12" style="color: #ec5500;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -26,7 +35,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">Paid, not Arrive</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+      <font-awesome-icon  :icon="faUserTag"  class="fa-solid fa-user mt-5 h-12" style="color: #1e6ecc;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -38,7 +47,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">City Name</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+      <font-awesome-icon  :icon="faUserClock"  class="fa-solid fa-user mt-5 h-12" style="color: #ec5500;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -50,7 +59,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">City Name</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+      <font-awesome-icon  :icon="faUserTag"  class="fa-solid fa-user mt-5 h-12" style="color: #1e6ecc;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -63,7 +72,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">City Name</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+      <font-awesome-icon  :icon="faUserGroup"  class="fa-solid fa-user mt-5 h-12" style="color: #ffb800;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -74,7 +83,7 @@ import UserTable from '../components/HomeView/UserTable.vue';
   <div>
     <h2 class="mb-2 text-xl font-semibold">City Name</h2>
     <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
+      <font-awesome-icon  :icon="faUserCheck"  class="fa-solid fa-user mt-5 h-12" style="color: #42ae00;"/>
       <p class="mt-4 text-right">
         <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
       </p>
@@ -82,14 +91,9 @@ import UserTable from '../components/HomeView/UserTable.vue';
   </div>
 </div>
 <div class="flex h-36 w-full rounded-lg bg-white p-4 shadow-md">
-  <div>
-    <h2 class="mb-2 text-xl font-semibold">City Name</h2>
-    <div class="flex">
-      <div class="mr-4 text-4xl mt-5 text-blue-500">🏙️</div>
-      <p class="mt-4 text-right">
-        <span class="ml-6 font-bold text-5xl text-blue-500">126</span>
-      </p>
-    </div>
+  <div class="mx-auto">
+      <font-awesome-icon  :icon="faCamera"  class="fa-solid fa-user mx-auto mt-5 h-12" style="color: #1e6ecc;"/>
+    <h2 class="mb-2 text-md font-semibold">Scan a Registree</h2>
   </div>
 </div>
 </div>
